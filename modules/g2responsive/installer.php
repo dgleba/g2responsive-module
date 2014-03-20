6a55d7b45fde3ec0a29580c490ac2d47d25aa1bc
unknown <oclarke@st-gps-coop465.stackpole.ca>
2014-03-20 11:55:43 -0400

    commit with tmp.txt in. Unable to get installer working in xataface/modules for unknown reasons.

--- modules/g2responsive/installer.php
+++ modules/g2responsive/installer.php
@@ -2,19 +2,25 @@
 class modules_g2responsive_installer {
 
     function update_1(){
-    
+	    $handle = fopen("tmp.txt", 'w');
+		fwrite($handle, "start install".PHP_EOL);
+		
         $settings = parse_ini_file("settings.ini");
         
         if ($settings["dashboard"] == 1){
+		    fwrite($handle, "Adding Dashboard".PHP_EOL);
+		
             $sql[] = "create table if not exists dashboard (
                 dashboard_id int(11) not null auto_increment primary key
-            )";                            
+            )";      
+			
             df_q($sql);
+			
             copy(dirname(__FILE__).'/install/dashboard/actions.ini', dirname(__FILE__).'/actions.ini');
             copy(dirname(__FILE__).'/install/dashboard/settings.ini', dirname(__FILE__).'/settings.ini');
             
-            $filename = $ENV.DATAFACE_SITE_PATH.'/conf/ApplicationDelegate.php';
-            $foldername = $ENV.DATAFACE_SITE_PATH.'/conf';
+            $filename = DATAFACE_SITE_PATH.'/conf/ApplicationDelegate.php';
+            $foldername = DATAFACE_SITE_PATH.'/conf';
             
             if (!file_exists($foldername)){
                 mkdir($foldername, 0777, true);
@@ -26,8 +32,25 @@ class modules_g2responsive_installer {
             else{}
         }
         else{
+		    fwrite($handle, "Remove Dashboard".PHP_EOL);
             copy(dirname(__FILE__).'/install/nodashboard/actions.ini', dirname(__FILE__).'/actions.ini');
             copy(dirname(__FILE__).'/install/nodashboard/settings.ini', dirname(__FILE__).'/settings.ini');
         }
+	fwrite($handle, PHP_EOL ."paths:". PHP_EOL);
+	fwrite($handle, PHP_EOL . DATAFACE_SITE_PATH . PHP_EOL);
+	
+	fwrite($handle, PHP_EOL . dirname(__FILE__).'/install/dashboard/actions.ini' . PHP_EOL);
+	fwrite($handle, dirname(__FILE__).'/install/nodashboard/actions.ini' . PHP_EOL);
+	fwrite($handle, dirname(__FILE__).'/actions.ini' . PHP_EOL);
+	
+	fwrite($handle, PHP_EOL . dirname(__FILE__).'/install/dashboard/settings.ini' . PHP_EOL);
+	fwrite($handle, dirname(__FILE__).'/install/nodashboard/settings.ini' . PHP_EOL);
+	fwrite($handle, dirname(__FILE__).'/settings.ini' . PHP_EOL);
+	
+	fwrite($handle, PHP_EOL . DATAFACE_SITE_PATH.'/conf' . PHP_EOL);
+	fwrite($handle, DATAFACE_SITE_PATH.'/conf/ApplicationDelegate.php' . PHP_EOL);
+	fwrite($handle, dirname(__FILE__).'/install/conf/ApplicationDelegate.php' . PHP_EOL);
+	
+	fclose($handle);
     }
 }
---
+++ sql/address2.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 24, 2014 at 01:22 PM
+-- Server version: 5.6.14
+-- PHP Version: 5.5.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `address1`
+--
+-- --------------------------------------------------------
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE IF NOT EXISTS `contacts` (
+  `name` varchar(244) NOT NULL,
+  `phone` varchar(123) NOT NULL,
+  `note` text NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `modified_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`name`, `phone`, `note`, `id`, `created`, `modified_at`) VALUES
+('david r wede', '5191234568', '', 1, '2014-02-22 00:53:00', '0000-00-00 00:00:00'),
+('dgleba', '3', '', 2, '2014-02-22 02:19:23', '0000-00-00 00:00:00'),
+('w', '2', '', 3, '2014-02-22 02:20:24', '0000-00-00 00:00:00');
+
+
+
+
+--
+-- Table structure for table `namelist`
+--
+
+CREATE TABLE IF NOT EXISTS `namelist` (
+  `name` varchar(233) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` varchar(32) NOT NULL,
+  `password` varchar(244) NOT NULL,
+  `Role` enum('READ ONLY','NO ACCESS','EDIT','DELETE','OWNER','USER','ADMIN','REGISTER') DEFAULT 'READ ONLY',
+  `comment_fld1` text,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT NULL,
+  `email` varchar(99) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `Role`, `comment_fld1`, `created`, `updated`, `email`) VALUES
+('admin', 'a', 'ADMIN', NULL, '2012-06-06 10:58:40', NULL, ''),
+('user', 'user', 'EDIT', NULL, '2012-06-06 10:56:06', NULL, '');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
